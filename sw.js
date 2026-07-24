a83d66dc311149e25ca431d9eebd65b68276f22e
Sense <36178427+sense-framework@users.noreply.github.com>
2026-07-24 17:57:18 -0400

    Refresh company platform offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sense-empty-v0.4.0';
+const CACHE='sense-company-v0.5.0';
 const ASSETS=['./','./index.html','./senseos.css','./senseos.js','./manifest.webmanifest','./icon.svg','./romeo.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
