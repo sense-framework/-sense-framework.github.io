e33e8319da8e09e3c24d0b570d25bbc5a67307e3
Sense <36178427+sense-framework@users.noreply.github.com>
2026-07-24 16:45:29 -0400

    Cache direct SenseOS assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sense-unified-v0.2.2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./romeo.js','./romeo-mobile.js'];
+const CACHE='sense-direct-v0.2.2';
+const ASSETS=['./','./index.html','./senseos.css','./senseos.js','./manifest.webmanifest','./icon.svg','./romeo.js','./romeo-mobile.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==location.origin)return;if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));return}event.respondWith(caches.match(event.request,{ignoreSearch:true}).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok)caches.open(CACHE).then(cache=>cache.put(event.request,response.clone()));return response})))});
